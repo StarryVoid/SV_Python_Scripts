c8a4722646590eda3cfdb05a07063c7b4f681220
StarryVoid <starrysky806@gmail.com>
2021-03-23 11:12:10 +0800

    整理配置参数集中管理，修改默认选项为不加载系统DNS文件
    默认配置选项 移动到最前侧
    默认选项 修改为不加载系统DNS文件
    默认选项 超时时间 2s 改为 1s 
    修改版本号为 v1.1.0

--- dynamic_domain-ipaddress_make_nginx_accesslist_config(white_list).py
+++ dynamic_domain-ipaddress_make_nginx_accesslist_config(white_list).py
@@ -5,30 +5,53 @@
 #
 # Author: StarryVoid <stars@starryvoid.com>
 # Intro:  https://blog.starryvoid.com/archives/585.html
-# Build:  2021/03/22 Version 1.0.1
+# Build:  2021/03/23 Version 1.1.0
 #
 # Operating environment "Python3 dnspython"
 # Install Command "pip3 install dnspython"
 # About "dnspython" https://github.com/rthalley/dnspython https://dnspython.readthedocs.io/en/latest/
 # About "ngx_http_access_module" http://nginx.org/en/docs/http/ngx_http_access_module.html
-#
+
+### Config Start ###
+
 # Default Input File ./dns_read_list.txt
-# One line of the configuration file is like "www.google.com#1.1.1.1#A#216.58.197.196#Annotation"
-#
+# One line of the configuration file is like "www.google.com#8.8.8.8#A#216.58.197.196#Annotation"
+# You can use commands like this to generate sample files " echo -e 'domain1#dnsserver1#A#ipaddress_old1#Annotation1\ndomain2#dnsserver2#A#ipaddress_old2#Annotation2' "
+
+Input_file_path = "./dns_read_list.txt"
+
 # Default Output File ./nginx_access_whitelist.conf
 # One line of the output file is like "Allow 216.58.197.196/32;"
 # There is an extra rule at the end of the configuration file "deny all;"
 # And automatically reload the Nginx service "systemctl reload nginx.service"
 
+Output_file_path = "./nginx_access_whitelist.conf"
+Auto_Reload_Nginx_Command = "systemctl reload nginx.service"
+
+# (bool)Read Your Linux System File '/etc/resolv.conf' , Default is '0' , DNSPython default is '1' .
+# If the '/etc/resolv.conf' file of your Linux server is damaged, Please configure to ‘0’ to disable this option to prevent errors
+# The errors result during the test is like this "dns.resolver.NoResolverConfiguration: Resolver configuration could not be read or specified no nameservers."
+DNS_Read_System_Config = "0"
+
+# (str/None)Your Network Drive Bind Address , Default is "None" , Change to "str("127.0.0.1")"
+DNS_Query_Source_Address = None
+
+# (list)Extended DNS server for additional queries , Default is "['8.8.8.8','1.1.1.1','1.2.4.8']"
+# When there is a DNS query error, you can select other public servers to query 
+DNS_Query_Server_Expand = ['1.1.1.1','8.8.8.8','114.114.114.114']
+
+
+### Config End ###
+
 import sys,dns.resolver,subprocess
 
 def DNS_Query(domain_name,dns_server,domain_type,source_address):
     try:
-        DNS_Resolver = dns.resolver.Resolver()
+        DNS_Resolver = dns.resolver.Resolver(configure=bool(DNS_Read_System_Config))
         DNS_Resolver.port = int("53")
-        DNS_Resolver.timeout = float("2.0")
+        DNS_Resolver.timeout = float("1.0")
         DNS_Resolver.lifetime = float("5.0")
-        DNS_Resolver.nameservers = list(str(dns_server).split('#'))
+        DNS_Resolver.nameservers = list(str(dns_server).split('#'))+list(DNS_Query_Server_Expand)
         return str(DNS_Resolver.resolve(str(domain_name),rdtype=str(domain_type),source=source_address).response.answer[-1].to_text().split("\n")[0].split(" ")[4])
         #
         #DNS_Answer_List=list()
@@ -54,13 +77,12 @@ def main():
         Input_tmp_data=[]
         Output_tmp_data=[]
         Difference_Status=int(0)
-        DNS_Query_Source_Address=None    #Your Network Bind Address , Default is "None" , Change to "str("127.0.0.1")"
         #
-        with open("dns_read_list.txt", "r") as r_file:
+        with open(str(Input_file_path), "r") as r_file:
             r_file_lines = r_file.readlines()
         #
         for r_line in r_file_lines:
-            #r_line="www.163.com#1.2.4.5#A#216.58.197.196#Annotation"
+            #r_line="www.google.com#8.8.8.8#A#216.58.197.196#Annotation"
             DNS_query_info=str(r_line).split('#')
             DNS_query_info[-1]=DNS_query_info[-1].replace('\n', '').replace('\r', '')
             #print("Inf1",DNS_query_info)
@@ -83,11 +105,11 @@ def main():
             Output_tmp_data.append('deny all;\n')
             #print("O1",Input_tmp_data)
             #print("O2",Output_tmp_data)
-            with open("dns_read_list.txt", "w+") as w_file_output:
+            with open(str(Input_file_path), "w+") as w_file_output:
                 w_file_output.write("\n".join(Input_tmp_data))
-            with open("nginx_access_whitelist.conf", "w+") as w_file_input:
+            with open(str(Output_file_path), "w+") as w_file_input:
                 w_file_input.write("\n".join(Output_tmp_data))
-            subprocess.Popen('systemctl reload nginx.service',shell=True).returncode
+            subprocess.Popen(str(Auto_Reload_Nginx_Command),shell=True).returncode
     #
     except Exception as Error:
         print ('[Error]: Some errors have occurred, please check the configuration file.')
