47380551a96263c16052aecbbb257518bca591bd
StarryVoid <20756365+StarryVoid@users.noreply.github.com>
2021-05-11 08:56:19 +0800

    Adjust code order
    Adjust code order
    Change 'DNS_Query_Server_Expand' default config

--- dynamic_domain-ipaddress_make_nginx_accesslist_config(white_list).py
+++ dynamic_domain-ipaddress_make_nginx_accesslist_config(white_list).py
@@ -12,6 +12,8 @@
 # About "dnspython" https://github.com/rthalley/dnspython https://dnspython.readthedocs.io/en/latest/
 # About "ngx_http_access_module" http://nginx.org/en/docs/http/ngx_http_access_module.html
 
+import sys,dns.resolver,subprocess
+
 ### Config Start ###
 
 # Default Input File ./dns_read_list.txt
@@ -24,25 +26,23 @@ Input_file_path = "./dns_read_list.txt"
 # There is an extra rule at the end of the configuration file "deny all;"
 # And automatically reload the Nginx service "systemctl reload nginx.service"
 Output_file_path = "./nginx_access_whitelist.conf"
-Auto_Reload_Nginx_Command = "systemctl reload nginx.service"
+Auto_Reload_Command = "systemctl reload nginx.service"
 
 # (bool)Read Your Linux System File '/etc/resolv.conf' , Default is '0' , DNSPython default is '1' .
 # If the '/etc/resolv.conf' file of your Linux server is damaged, Please configure to ‘0’ to disable this option to prevent errors
 # The errors result during the test is like this "dns.resolver.NoResolverConfiguration: Resolver configuration could not be read or specified no nameservers."
 DNS_Read_System_Config = "0"
 
-# (str/None)Your Network Drive Bind Address , Default is "None" , Change to "str("127.0.0.1")"
+# (str/None)Your Network Drive Bind Address , Default is "None" , Change to "str('127.0.0.1')"
 DNS_Query_Source_Address = None
 
-# (list)Extended DNS server for additional queries , Default is "['8.8.8.8','1.1.1.1','1.2.4.8']"
+# (list)Extended DNS server for additional queries , Default is "['8.8.8.8','1.1.1.1','114.114.114.114']"
 # When there is a DNS query error, you can select other public servers to query 
-DNS_Query_Server_Expand = ['1.1.1.1','8.8.8.8','114.114.114.114']
+DNS_Query_Server_Expand = ['8.8.8.8','1.1.1.1','114.114.114.114']
 
 ### Config End ###
 
 ### Script start ###
-
-import sys,dns.resolver,subprocess
 
 def DNS_Query(domain_name,dns_server,domain_type,source_address):
     try:
@@ -53,24 +53,25 @@ def DNS_Query(domain_name,dns_server,domain_type,source_address):
         DNS_Resolver.nameservers = list(str(dns_server).split('#'))+list(DNS_Query_Server_Expand)
         DNS_Response = sorted(answer.split(" ")[4].strip(".").lower() for answer in DNS_Resolver.resolve(str(domain_name),rdtype=str(domain_type),source=source_address).response.answer[-1].to_text().split("\n"))
         return DNS_Response[0]
-        #
-        #Other data processing methods 
-        #
-        #DNS_Answer_List=list()
-        #DNS_Answer_List_Rdata = DNS_Resolver.resolve(str(domain_name),str(domain_type))
-        #for rdata in DNS_Answer_List_Rdata:
-        #    DNS_Answer_List.append(str(rdata))                              #IP
-        #    DNS_Answer_List.append(str(rdata.target).strip(".").lower())    #CNAME
-        #return tuple(DNS_Answer_List)
-        #
-        #DNS_Answer_List=tuple()
-        #DNS_Answer_List_RRset = DNS_Resolver.resolve(str(domain_name),str(domain_type))
-        #for rrset in DNS_Answer_List_RRset.response.answer:
-        #    for i in rrset.items:
-        #        print(i.to_text())
-        #        DNS_Answer_List.append(str(i.to_text()).strip(".").lower())
-        #return tuple(DNS_Answer_List)
-        #
+        """
+        Other data processing methods 
+        
+        DNS_Answer_List=list()
+        DNS_Answer_List_Rdata = DNS_Resolver.resolve(str(domain_name),str(domain_type))
+        for rdata in DNS_Answer_List_Rdata:
+            DNS_Answer_List.append(str(rdata))                              #IP
+            #DNS_Answer_List.append(str(rdata.target).strip(".").lower())    #CNAME
+        return tuple(DNS_Answer_List)
+        """
+        """
+        DNS_Answer_List=tuple()
+        DNS_Answer_List_RRset = DNS_Resolver.resolve(str(domain_name),str(domain_type))
+        for rrset in DNS_Answer_List_RRset.response.answer:
+            for i in rrset.items:
+                print(i.to_text())
+                DNS_Answer_List.append(str(i.to_text()).strip(".").lower())
+        return tuple(DNS_Answer_List)
+        """
     except Exception as Error:
         print (domain_name,domain_type,'Error: unable to start def \"DNS_Query\"')
 
@@ -112,7 +113,7 @@ def main():
                 w_file_output.write("\n".join(Input_tmp_data))
             with open(str(Output_file_path), "w+") as w_file_input:
                 w_file_input.write("\n".join(Output_tmp_data))
-            subprocess.Popen(str(Auto_Reload_Nginx_Command),shell=True).returncode
+            subprocess.Popen(str(Auto_Reload_Command),shell=True).returncode
     #
     except Exception as Error:
         print ('[Error]: Some errors have occurred, please check the configuration file.')
